528e82ec0303690ceb27b07a813083e7ab46a6c5
thongbuind <thongbuind2005@gmail.com>
2024-06-24 00:09:27 +0700

    start

---
+++ header.h
@@ -0,0 +1,32 @@
+#ifndef HEADER_H
+#define HEADER_H
+
+#include <stdio.h>
+#include <string.h>
+
+
+struct product {
+    string name;
+    int quantity;
+    int price;
+};
+
+struct manage {
+    string name_product;
+    int quantity;
+    string name_staff;
+};
+
+
+// Khai báo các hàm của Thông
+
+// Khai báo các hàm của Thành
+
+// Khai báo các hàm của Long
+
+// Khai báo các hàm của Thiên
+
+
+
+
+#endif // HEADER_H
---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <string.h>
+#include "header.h"
+
+int main() {
+    printf("hello world");
+}
